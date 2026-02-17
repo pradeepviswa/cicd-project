5439bffb6d2b01420cb0c7c4b461cb0241a4addd
Pradeep Viswanathan <pradeep.viswa@gmail.com>
2026-02-17 22:23:56 +0530

    apply and destroy code adde

--- destroy_vm.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_instance" "vm" {
-  count = length(var.instance_ids_to_destroy)
-}
--- variable.tf
+++ variable.tf
@@ -3,3 +3,8 @@ variable "instance_ids_to_destroy" {
   type        = list(string)
   default     = []
 }
+
+variable "instance_type" {
+  description = "Instance type"
+  type        = string
+}
