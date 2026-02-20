0e3b5757a7e363c13c8c756b00c90242268e3b02
Pradeep Viswanathan <pradeep.viswa@gmail.com>
2026-02-20 17:39:28 +0530

    updated tf

--- terraform/create_vm/backend.tf
+++ terraform/create_vm/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-tf-bucket-1726"
     key            = "ec2/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-lock"
   }
 }
